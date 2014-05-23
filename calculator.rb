3867858483356eda09b20f4ee12c6fd224247f5c
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-05-23 14:27:51 -0600

    added code to calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -1,19 +1,26 @@
-puts "choose a number"
-first_num = gets.chomp.to_i
-puts "choose another number"
-second_num = gets.chomp.to_i
+operation = "+, -, /, *"
 
-puts "Which operation would you like to use?( + , - , / , *)"
-operation = gets.chomp
+while operation != "+" && operation != "-" && operation != "/" && operation != "*"
 
-if operation == "+"
-  answer = first_num + second_num
-elsif operation == "-"
-  answer = first_num - second_num
-elsif operation == "/"
-  answer = first_num / second_num
-elsif operation == "*"
-  answer - first_num * second_num
+  puts "choose a number"
+  first_num = gets.chomp.to_f
+  puts "choose another number"
+  second_num = gets.chomp.to_f
+
+  puts "Which operation would you like to use?( + , - , / , *)"
+  operation = gets.chomp
+
+  if operation == "+"
+    answer = first_num + second_num
+  elsif operation == "-"
+    answer = first_num - second_num
+  elsif operation == "/"
+    answer = first_num / second_num
+  elsif operation == "*"
+    answer - first_num * second_num
+  else
+    puts "I cannot calculate that
+    please try again"
+  end
 end
-
   puts "This is your answer #{answer}"
